852c7f86b973d196ff3aeed25a36cd6d9edd6448
Abdifatah.206 <shire2567@gmail.com>
2019-05-07 12:11:52 -0700

    Upadated onclick listview to secondactivity more detail

---
+++ app/src/main/java/com/example/myad340/traffic_camera.java
@@ -0,0 +1,13 @@
+package com.example.myad340;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class traffic_camera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_camera);
+    }
+}
