80bf9cb93e763b1d43f82ab3b8cc11292f6460ec
Abdifatah.206 <shire2567@gmail.com>
2019-05-11 05:28:31 -0700

    added up about us page

--- app/src/main/java/com/example/myad340/MainActivity.java
+++ app/src/main/java/com/example/myad340/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -17,6 +20,7 @@ import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 import static android.view.View.*;
+import static com.example.myad340.R.menu.about;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,8 +32,10 @@ public class MainActivity extends AppCompatActivity {
     Button west;
     Button south;
     Button north;
+    Button roadbtn;
     String mystring = "welcome North";
     EditText edit;
+    //Menu menu;
     public void init() {
         btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(new OnClickListener() {
@@ -54,7 +60,32 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
+    public void road() {
+        roadbtn = (Button) findViewById(R.id.camers);
+        roadbtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent E = new Intent(MainActivity.this, traffic_camera.class);
 
+                startActivity(E);
+            }
+
+        });
+    }
+    /*
+    public void road1() {
+        menu = (Menu) findViewById(R.id.item1);
+        roadbtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent E = new Intent(MainActivity.this, traffic_camera.class);
+
+                startActivity(E);
+            }
+
+        });
+    }
+*/
 
 
 
@@ -63,13 +94,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         east1();
+        road();
+       // road1();
 
 
 
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.about,menu);
 
+       // Intent E = new Intent(MainActivity.this, traffic_camera.class);
+
+       // startActivity(E);
+
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent E = new Intent(MainActivity.this, About.class);
+
+                startActivity(E);
+                //Toast.makeText(this,"item 1 selected",Toast.LENGTH_SHORT).show();
+        return true;
+        default:
+            return super.onOptionsItemSelected(item);
+
+        }
+
+    }
 
     /** Called when the user taps the Send button */
 
@@ -102,12 +163,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Do something in response to button
     }
+    /*
     public void west(View view) {
 
         Toast.makeText(MainActivity.this,"H E L L O  W E S T ", Toast.LENGTH_LONG).show();
 
         // Do something in response to button
     }
+    */
     public void north(View view) {
 
         Toast.makeText(MainActivity.this,"H E L L O  N O R T H", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/myad340/detail.java
+++ app/src/main/java/com/example/myad340/detail.java
@@ -1,14 +1,45 @@
 package com.example.myad340;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+
 
 public class detail extends AppCompatActivity {
+ TextView titles;
+ TextView years;
+ TextView directors;
+
+ //ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_detail);
+        titles = findViewById(R.id.d_title);
+        years = findViewById(R.id.d_year);
+        directors = findViewById(R.id.d_director);
+        //image = findViewById(R.id.d_image);
+        Intent intent = getIntent();
+        titles.setText(intent.getStringExtra("titles"));
+        years.setText(intent.getStringExtra("years"));
+        directors.setText(intent.getStringExtra("directors"));
+        //image.setImageResource(intent.getIntExtra("image", 0));
+
+
+       // String value = getIntent().getExtras().getString(movies);
+
+       // textView =(TextView) findViewById(R.id.textitem);
+
+       //String tempholder = getIntent().getStringExtra("movies");
+       //textView.setText(tempholder);
+        //Bundle bundle = getIntent().getExtras();
+        //String data = bundle.getString("movies");
+
 
     }
 }
