eb913e9c5bfc07072887de1de0c2c7d5f529cc7a
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-05-30 14:05:22 -0700

    added app navbar with up navigation

--- app/src/main/java/com/example/myad340/East.java
+++ app/src/main/java/com/example/myad340/East.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,6 +47,8 @@ public class East extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.east);
+
+
         //listdetail();
         listView = (ListView) findViewById(R.id.movie_list);
 
--- app/src/main/java/com/example/myad340/detail.java
+++ app/src/main/java/com/example/myad340/detail.java
@@ -3,6 +3,7 @@ package com.example.myad340;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +21,7 @@ public class detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
         //titles = findViewById(R.id.d_title);
 
         //years = findViewById(R.id.d_year);
